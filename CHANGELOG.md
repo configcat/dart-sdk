ad4d5bfc6277a607499dc874cb8e05a6bcc42ea2
Peter Csajtai <peter.csajtai@outlook.com>
2023-06-21 16:43:45 +0200

    Drop logger dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 2.5.2 - 2023-06-21
+### Removed
+- `logger` package dependency. Switched to simple `print()` as default.
+
 ## 2.5.1 - 2023-06-21
 ### Changed
 - Bump to `dio v5.2.0` in order to use `DioException` instead of the deprecated `DioError`.
--- lib/src/log/default_logger.dart
+++ lib/src/log/default_logger.dart
@@ -1,43 +1,33 @@
-import 'package:logger/logger.dart' as ext;
-
 import 'logger.dart';
 
 /// Default [Logger] implementation used by [ConfigCatLogger].
 class DefaultLogger implements Logger {
-  late final ext.Logger _internal;
-
-  DefaultLogger({
-    ext.Logger? internalLogger,
-  }) {
-    _internal = internalLogger ??
-        ext.Logger(
-            level: ext.Level.verbose,
-            filter: ext.ProductionFilter(),
-            printer: ext.SimplePrinter(printTime: true));
-  }
-
   @override
   void debug(message, [dynamic error, StackTrace? stackTrace]) {
-    _internal.d(message, error, stackTrace);
+    _log("[DEBUG]", message, error, stackTrace);
   }
 
   @override
   void error(message, [dynamic error, StackTrace? stackTrace]) {
-    _internal.e(message, error, stackTrace);
+    _log("[ERROR]", message, error, stackTrace);
   }
 
   @override
   void info(message, [dynamic error, StackTrace? stackTrace]) {
-    _internal.i(message, error, stackTrace);
+    _log("[INFO]", message, error, stackTrace);
   }
 
   @override
   void warning(message, [dynamic error, StackTrace? stackTrace]) {
-    _internal.w(message, error, stackTrace);
+    _log("[WARN]", message, error, stackTrace);
   }
 
   @override
-  void close() {
-    _internal.close();
+  void close() {}
+
+  void _log(String prefix, dynamic message,
+      [dynamic error, StackTrace? stackTrace]) {
+    var err = error != null ? ' ERROR: $error' : '';
+    print('$prefix ${DateTime.now().toIso8601String()}: $message$err');
   }
 }
