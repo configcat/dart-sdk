2d7a9bced3fc4c06bbefc77a2a4e421c0ef24bad
Peter Csajtai <peter.csajtai@outlook.com>
2023-08-18 13:38:09 +0200

    Update constants.dart

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -3,4 +3,4 @@ const configJsonCacheVersion = 'v2';
 const configJsonName = 'config_v5.json';
 final DateTime distantPast = DateTime.utc(1970, 01, 01);
 final DateTime distantFuture =
-    DateTime.now().toUtc().add(const Duration(days: 1000));
+    DateTime.now().toUtc().add(const Duration(days: 1000 * 365));
