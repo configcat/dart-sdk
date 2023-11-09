37eff4a37143c4399b47d5194c2fb1becee4730e
novalisdenahi <novalisdenahi@gmail.com>
2023-11-09 11:59:54 +0100

    Comparators rework

--- lib/src/rollout_evaluator.dart
+++ lib/src/rollout_evaluator.dart
@@ -23,28 +23,82 @@ class EvaluationResult<T> {
       required this.matchedEvaluationRule,
       required this.matchedEvaluationPercentageRule});
 }
+enum SegmentComparator {
+  isInSegment(id: 0,name:  "IS IN SEGMENT"),
+  isNotInSegment(id: 1,name: "IS NOT IN SEGMENT");
+
+  final int id;
+  final String name;
+
+  const SegmentComparator({
+    required this.id,
+    required this.name
+  });
+}
+
+enum PrerequisiteComparator {
+  equals(id: 0, name: "EQUALS"),
+  notEquals(id: 1, name: "NOT EQUALS");
+
+  final int id;
+  final String name;
+
+  const PrerequisiteComparator({
+    required this.id,
+    required this.name
+  });
+}
+
+enum UserComparator {
+  isOneOf(id: 0,name: "IS ONE OF"),
+  isNotOneOf(id: 1,  name:"IS NOT ONE OF"),
+  containsAnyOf(id:2,  name:"CONTAINS ANY OF"),
+  notContainsAnyOf(id:3,  name:"NOT CONTAINS ANY OF"),
+  semverIsOneOf(id:4,  name:"IS ONE OF"),
+  semverIsNotOneOf(id:5,  name:"IS NOT ONE OF"),
+  semverLess(id:6,  name:"<"),
+  semverLessEquals(id:7,  name:"<="),
+  semverGreater(id:8,  name:">"),
+  semverGreaterEquals(id:9,  name:">="),
+  numberEquals(id:10,  name:"="),
+  numberNotEquals(id:11,  name:"!="),
+  numberLess(id:12, name: "<"),
+  numberLessEquals(id: 13,  name:"<="),
+  numberGreater(id:14,  name:">"),
+  numberGreaterEquals(id:15,  name:">="),
+  sensitiveIsOneOf(id:16,  name:"IS ONE OF"),
+  sensitiveIsNotOneOf(id:17, name: "IS NOT ONE OF"),
+  dateBefore(id:18, name: "BEFORE"),
+  dateAfter(id:19,  name:"AFTER"),
+  hashedEquals(id:20,  name:"EQUALS"),
+  hashedNotEquals(id:21, name: "NOT EQUALS"),
+  hashedStartsWith(id:22,  name:"STARTS WITH ANY OF"),
+  hashedNotStartsWith(id:23,  name:"NOT STARTS WITH ANY OF"),
+  hashedEndsWith(id:24, name: "ENDS WITH ANY OF"),
+  hashedNotEndsWith(id:25,  name:"NOT ENDS WITH ANY OF"),
+  hashedArrayContains(id:26,  name:"ARRAY CONTAINS ANY OF"),
+  hashedArrayNotContains(id:27,  name:"ARRAY NOT CONTAINS ANY OF"),
+  textEquals(id:28, name: "EQUALS"),
+  textNotEquals(id:29, name: "NOT EQUALS"),
+  textStartsWith(id:30,  name:"STARTS WITH ANY OF"),
+  textNotStartsWith(id:31,  name:"NOT STARTS WITH ANY OF"),
+  textEndsWith(id:32,  name:"ENDS WITH ANY OF"),
+  textNotEndsWith(id:33,  name:"NOT ENDS WITH ANY OF"),
+  textArrayContains(id:34, name: "ARRAY CONTAINS ANY OF"),
+  textArrayNotContains(id:35, name: "ARRAY NOT CONTAINS ANY OF");
+
+  final int id;
+  final String name;
+
+  const UserComparator({
+    required this.id,
+    required this.name
+  });
+}
+
+
 
 class RolloutEvaluator {
-  static const _comparatorTexts = [
-    'IS ONE OF',
-    'IS NOT ONE OF',
-    'CONTAINS',
-    'DOES NOT CONTAIN',
-    'IS ONE OF (SemVer)',
-    'IS NOT ONE OF (SemVer)',
-    '< (SemVer)',
-    '<= (SemVer)',
-    '> (SemVer)',
-    '>= (SemVer)',
-    '= (Number)',
-    '<> (Number)',
-    '< (Number)',
-    '<= (Number)',
-    '> (Number)',
-    '>= (Number',
-    'IS ONE OF (Sensitive)',
-    'IS NOT ONE OF (Sensitive)',
-  ];
 
   final ConfigCatLogger _logger;
 
@@ -83,80 +137,29 @@ class RolloutEvaluator {
       for (final rule in setting.rolloutRules) {
         final comparisonAttribute = rule.comparisonAttribute;
         final comparisonValue = rule.comparisonValue;
-        final comparator = rule.comparator;
+        final comparator = UserComparator.values.firstWhere((element) => element.id == rule.comparator);
         final userValue = user.getAttribute(comparisonAttribute);
         final returnValue = rule.value as Value;
 
         if (userValue == null || comparisonValue.isEmpty || userValue.isEmpty) {
-          logEntries.add(_formatNoMatchRule(
-              comparisonAttribute: comparisonAttribute,
-              userValue: userValue ?? '',
-              comparator: comparator,
-              comparisonValue: comparisonValue));
           continue;
         }
 
         switch (comparator) {
-          // IS ONE OF
-          case 0:
-            final split =
-                comparisonValue.split(',').map((value) => value.trim());
-            if (split.contains(userValue)) {
-              logEntries.add(_formatMatchRule(
-                  comparisonAttribute: comparisonAttribute,
-                  userValue: userValue,
-                  comparator: comparator,
-                  comparisonValue: comparisonValue,
-                  value: returnValue));
-              return produceResult(rolloutRule: rule);
-            }
-            break;
-          // IS NOT ONE OF
-          case 1:
-            final split =
-                comparisonValue.split(',').map((value) => value.trim());
-            if (!split.contains(userValue)) {
-              logEntries.add(_formatMatchRule(
-                  comparisonAttribute: comparisonAttribute,
-                  userValue: userValue,
-                  comparator: comparator,
-                  comparisonValue: comparisonValue,
-                  value: returnValue));
-              return produceResult(rolloutRule: rule);
-            }
-            break;
-          // CONTAINS
-          case 2:
+          case UserComparator.containsAnyOf:
+          case UserComparator.notContainsAnyOf:
+            bool negateContainsAnyOf = UserComparator.notContainsAnyOf == comparator;
+            //TODO fix me. we need the new models to implement properly the changes
             if (userValue.contains(comparisonValue)) {
-              logEntries.add(_formatMatchRule(
-                  comparisonAttribute: comparisonAttribute,
-                  userValue: userValue,
-                  comparator: comparator,
-                  comparisonValue: comparisonValue,
-                  value: returnValue));
               return produceResult(rolloutRule: rule);
             }
             break;
-          // DOES NOT CONTAIN
-          case 3:
-            if (!userValue.contains(comparisonValue)) {
-              logEntries.add(_formatMatchRule(
-                  comparisonAttribute: comparisonAttribute,
-                  userValue: userValue,
-                  comparator: comparator,
-                  comparisonValue: comparisonValue,
-                  value: returnValue));
-              return produceResult(rolloutRule: rule);
-            }
-            break;
-          // IS ONE OF (Semantic version), IS NOT ONE OF (Semantic version)
-          case 4:
-          case 5:
+          case UserComparator.semverIsOneOf:
+          case UserComparator.semverIsNotOneOf:
             final split = comparisonValue
                 .split(',')
                 .map((value) => value.trim())
                 .where((value) => value.isNotEmpty);
-
             try {
               final userVersion = _parseVersion(userValue);
               var matched = false;
@@ -164,17 +167,12 @@ class RolloutEvaluator {
                 matched = _parseVersion(value) == userVersion || matched;
               }
 
-              if ((matched && comparator == 4) ||
-                  (!matched && comparator == 5)) {
-                logEntries.add(_formatMatchRule(
-                    comparisonAttribute: comparisonAttribute,
-                    userValue: userValue,
-                    comparator: comparator,
-                    comparisonValue: comparisonValue,
-                    value: returnValue));
+              if ((matched && comparator == UserComparator.semverIsOneOf) ||
+                  (!matched && comparator == UserComparator.semverIsNotOneOf)) {
                 return produceResult(rolloutRule: rule);
               }
             } catch (e) {
+              //TODO fix message
               final message = _formatValidationErrorRule(
                   comparisonAttribute: comparisonAttribute,
                   userValue: userValue,
@@ -185,28 +183,23 @@ class RolloutEvaluator {
               logEntries.add(message);
             }
             break;
-          // LESS THAN, LESS THAN OR EQUALS TO, GREATER THAN, GREATER THAN OR EQUALS TO (Semantic version)
-          case 6:
-          case 7:
-          case 8:
-          case 9:
+          case UserComparator.semverLess:
+          case UserComparator.semverLessEquals:
+          case UserComparator.semverGreater:
+          case UserComparator.semverGreaterEquals:
             try {
               final userValueVersion = _parseVersion(userValue);
               final comparisonVersion = _parseVersion(comparisonValue.trim());
 
-              if ((comparator == 6 && userValueVersion < comparisonVersion) ||
-                  (comparator == 7 && userValueVersion <= comparisonVersion) ||
-                  (comparator == 8 && userValueVersion > comparisonVersion) ||
-                  (comparator == 9 && userValueVersion >= comparisonVersion)) {
-                logEntries.add(_formatMatchRule(
-                    comparisonAttribute: comparisonAttribute,
-                    userValue: userValue,
-                    comparator: comparator,
-                    comparisonValue: comparisonValue,
-                    value: returnValue));
+              if ((comparator == UserComparator.semverLess && userValueVersion < comparisonVersion) ||
+                  (comparator == UserComparator.semverLessEquals && userValueVersion <= comparisonVersion) ||
+                  (comparator == UserComparator.semverGreater && userValueVersion > comparisonVersion) ||
+                  (comparator == UserComparator.semverGreaterEquals && userValueVersion >= comparisonVersion)) {
+
                 return produceResult(rolloutRule: rule);
               }
             } catch (e) {
+              //TODO fix message
               final message = _formatValidationErrorRule(
                   comparisonAttribute: comparisonAttribute,
                   userValue: userValue,
@@ -217,31 +210,26 @@ class RolloutEvaluator {
               logEntries.add(message);
             }
             break;
-          case 10:
-          case 11:
-          case 12:
-          case 13:
-          case 14:
-          case 15:
+          case UserComparator.numberEquals:
+          case UserComparator.numberNotEquals:
+          case UserComparator.numberLess:
+          case UserComparator.numberLessEquals:
+          case UserComparator.numberGreater:
+          case UserComparator.numberGreaterEquals:
             try {
               final uvDouble = double.parse(userValue.replaceAll(',', '.'));
               final cvDouble =
                   double.parse(comparisonValue.replaceAll(',', '.'));
-              if ((comparator == 10 && uvDouble == cvDouble) ||
-                  (comparator == 11 && uvDouble != cvDouble) ||
-                  (comparator == 12 && uvDouble < cvDouble) ||
-                  (comparator == 13 && uvDouble <= cvDouble) ||
-                  (comparator == 14 && uvDouble > cvDouble) ||
-                  (comparator == 15 && uvDouble >= cvDouble)) {
-                logEntries.add(_formatMatchRule(
-                    comparisonAttribute: comparisonAttribute,
-                    userValue: userValue,
-                    comparator: comparator,
-                    comparisonValue: comparisonValue,
-                    value: returnValue));
+              if ((comparator == UserComparator.numberEquals && uvDouble == cvDouble) ||
+                  (comparator == UserComparator.numberNotEquals && uvDouble != cvDouble) ||
+                  (comparator == UserComparator.numberLess && uvDouble < cvDouble) ||
+                  (comparator == UserComparator.numberLessEquals && uvDouble <= cvDouble) ||
+                  (comparator == UserComparator.numberGreater && uvDouble > cvDouble) ||
+                  (comparator == UserComparator.numberGreaterEquals && uvDouble >= cvDouble)) {
                 return produceResult(rolloutRule: rule);
               }
             } catch (e) {
+              //TODO fix message
               final message = _formatValidationErrorRule(
                   comparisonAttribute: comparisonAttribute,
                   userValue: userValue,
@@ -252,8 +240,11 @@ class RolloutEvaluator {
               logEntries.add(message);
             }
             break;
-          // IS ONE OF (Sensitive)
-          case 16:
+          case UserComparator.isOneOf:
+          case UserComparator.isNotOneOf:
+          case UserComparator.sensitiveIsOneOf:
+          case UserComparator.sensitiveIsNotOneOf:
+            //TODO refactor - sensitive is not one of as well
             final split = comparisonValue
                 .split(',')
                 .map((value) => value.trim())
@@ -261,34 +252,41 @@ class RolloutEvaluator {
             final userValueHash =
                 sha1.convert(utf8.encode(userValue)).toString();
             if (split.contains(userValueHash)) {
-              logEntries.add(_formatMatchRule(
-                  comparisonAttribute: comparisonAttribute,
-                  userValue: userValue,
-                  comparator: comparator,
-                  comparisonValue: comparisonValue,
-                  value: returnValue));
               return produceResult(rolloutRule: rule);
             }
             break;
-          // IS NOT ONE OF (Sensitive)
-          case 17:
-            final split = comparisonValue
-                .split(',')
-                .map((value) => value.trim())
-                .where((value) => value.isNotEmpty);
-            final userValueHash =
-                sha1.convert(utf8.encode(userValue)).toString();
-            if (!split.contains(userValueHash)) {
-              logEntries.add(_formatMatchRule(
-                  comparisonAttribute: comparisonAttribute,
-                  userValue: userValue,
-                  comparator: comparator,
-                  comparisonValue: comparisonValue,
-                  value: returnValue));
-              return produceResult(rolloutRule: rule);
-            }
+          case UserComparator.dateBefore:
+          case UserComparator.dateAfter:
+            //TODO implement
+            break;
+          case UserComparator.textEquals:
+          case UserComparator.textNotEquals:
+          case UserComparator.hashedEquals:
+          case UserComparator.hashedNotEquals:
+            //TODO implement
+            break;
+          case UserComparator.hashedStartsWith:
+          case UserComparator.hashedNotStartsWith:
+          case UserComparator.hashedEndsWith:
+          case UserComparator.hashedNotEndsWith:
+            //TODO implement
+            break;
+          case UserComparator.textStartsWith:
+          case UserComparator.textNotStartsWith:
+            //TODO implement
+            break;
+          case UserComparator.textEndsWith:
+          case UserComparator.textNotEndsWith:
+            //TODO implement
+            break;
+          case UserComparator.textArrayContains:
+          case UserComparator.textArrayNotContains:
+          case UserComparator.hashedArrayContains:
+          case UserComparator.hashedArrayNotContains:
+            //TODO implement
             break;
           default:
+            //TODO fix messages
             logEntries.add(_formatNoMatchRule(
                 comparisonAttribute: comparisonAttribute,
                 userValue: userValue,
@@ -323,27 +321,27 @@ class RolloutEvaluator {
   String _formatMatchRule<Value>(
       {required String comparisonAttribute,
       required String userValue,
-      required int comparator,
+      required UserComparator comparator,
       required String comparisonValue,
       required Value? value}) {
-    return 'Evaluating rule: [$comparisonAttribute:$userValue] [${RolloutEvaluator._comparatorTexts[comparator]}] [$comparisonValue] => match, returning: $value';
+    return 'Evaluating rule: [$comparisonAttribute:$userValue] [${comparator.name}] [$comparisonValue] => match, returning: $value';
   }
 
   String _formatNoMatchRule(
       {required String comparisonAttribute,
       required String userValue,
-      required int comparator,
+      required UserComparator comparator,
       required String comparisonValue}) {
-    return 'Evaluating rule: [$comparisonAttribute:$userValue] [${RolloutEvaluator._comparatorTexts[comparator]}] [$comparisonValue] => no match';
+    return 'Evaluating rule: [$comparisonAttribute:$userValue] [${comparator.name}] [$comparisonValue] => no match';
   }
 
   String _formatValidationErrorRule(
       {required String comparisonAttribute,
       required String userValue,
-      required int comparator,
+      required UserComparator comparator,
       required String comparisonValue,
       required dynamic error}) {
-    return 'Evaluating rule: [$comparisonAttribute:$userValue] [${RolloutEvaluator._comparatorTexts[comparator]}] [$comparisonValue] => Skip rule. Validation error: $error';
+    return 'Evaluating rule: [$comparisonAttribute:$userValue] [${comparator.name}] [$comparisonValue] => Skip rule. Validation error: $error';
   }
 
   Version _parseVersion(String text) {
