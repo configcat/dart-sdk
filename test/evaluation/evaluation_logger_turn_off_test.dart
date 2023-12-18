5eea1ca7214c3ebf7612d0a99b294012b20a1235
novalisdenahi <novalisdenahi@gmail.com>
2023-12-19 00:37:16 +0100

    Format

--- test/evaluation/evaluation_logger_turn_off_test.dart
+++ test/evaluation/evaluation_logger_turn_off_test.dart
@@ -6,7 +6,6 @@ import 'package:test/test.dart';
 import 'evaluation_test_logger.dart';
 
 void main() {
-
   tearDown(() {
     ConfigCatClient.closeAll();
   });
@@ -19,14 +18,15 @@ void main() {
         sdkKey: "PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A",
         options: ConfigCatOptions(
           logger:
-          ConfigCatLogger(internalLogger: testLogger, level: LogLevel.info),
+              ConfigCatLogger(internalLogger: testLogger, level: LogLevel.info),
           pollingMode: PollingMode.manualPoll(),
         ));
 
     await client.forceRefresh();
 
     // Act
-    final String value = await client.getValue(key: 'stringContainsDogDefaultCat', defaultValue: 'default');
+    final String value = await client.getValue(
+        key: 'stringContainsDogDefaultCat', defaultValue: 'default');
 
     // Assert
     expect(value, equals('Cat'));
@@ -35,7 +35,6 @@ void main() {
     expect(2, equals(logList.length));
     expect(LogLevel.warning, equals(logList[0].logLevel));
     expect(LogLevel.info, equals(logList[0].logLevel));
-
   });
 
   test('evaluation log level warning', () async {
@@ -45,15 +44,16 @@ void main() {
     final client = ConfigCatClient.get(
         sdkKey: "PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A",
         options: ConfigCatOptions(
-          logger:
-          ConfigCatLogger(internalLogger: testLogger, level: LogLevel.warning),
+          logger: ConfigCatLogger(
+              internalLogger: testLogger, level: LogLevel.warning),
           pollingMode: PollingMode.manualPoll(),
         ));
 
     await client.forceRefresh();
 
     // Act
-    final String value = await client.getValue(key: 'stringContainsDogDefaultCat', defaultValue: 'default');
+    final String value = await client.getValue(
+        key: 'stringContainsDogDefaultCat', defaultValue: 'default');
 
     // Assert
     expect(value, equals('Cat'));
