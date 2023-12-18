a5d59016c3c5d11224363b48be579e9e2f5c32e1
novalisdenahi <novalisdenahi@gmail.com>
2023-12-19 00:36:58 +0100

    Evaluation Logger turn off test added

---
+++ test/evaluation/evaluation_logger_turn_off_test.dart
@@ -0,0 +1,65 @@
+import 'package:configcat_client/configcat_client.dart';
+
+import 'package:test/scaffolding.dart';
+import 'package:test/test.dart';
+
+import 'evaluation_test_logger.dart';
+
+void main() {
+
+  tearDown(() {
+    ConfigCatClient.closeAll();
+  });
+
+  test('evaluation log level info', () async {
+    // Arrange
+    final testLogger = EvaluationTestLogger();
+
+    final client = ConfigCatClient.get(
+        sdkKey: "PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A",
+        options: ConfigCatOptions(
+          logger:
+          ConfigCatLogger(internalLogger: testLogger, level: LogLevel.info),
+          pollingMode: PollingMode.manualPoll(),
+        ));
+
+    await client.forceRefresh();
+
+    // Act
+    final String value = await client.getValue(key: 'stringContainsDogDefaultCat', defaultValue: 'default');
+
+    // Assert
+    expect(value, equals('Cat'));
+
+    var logList = testLogger.getLogList();
+    expect(2, equals(logList.length));
+    expect(LogLevel.warning, equals(logList[0].logLevel));
+    expect(LogLevel.info, equals(logList[0].logLevel));
+
+  });
+
+  test('evaluation log level warning', () async {
+    // Arrange
+    final testLogger = EvaluationTestLogger();
+
+    final client = ConfigCatClient.get(
+        sdkKey: "PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A",
+        options: ConfigCatOptions(
+          logger:
+          ConfigCatLogger(internalLogger: testLogger, level: LogLevel.warning),
+          pollingMode: PollingMode.manualPoll(),
+        ));
+
+    await client.forceRefresh();
+
+    // Act
+    final String value = await client.getValue(key: 'stringContainsDogDefaultCat', defaultValue: 'default');
+
+    // Assert
+    expect(value, equals('Cat'));
+
+    var logList = testLogger.getLogList();
+    expect(1, equals(logList.length));
+    expect(LogLevel.warning, equals(logList[0].logLevel));
+  });
+}
